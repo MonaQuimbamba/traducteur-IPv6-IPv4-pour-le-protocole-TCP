82d0211169878873493c7b256ddc5568e9a7be45
mona-quimamba <claudio.antonio@etu.unilim.fr>
2022-04-15 06:46:01 +0200

    ajouter ipv62ipv4

--- source/analyse_paquets.py
+++ source/analyse_paquets.py
@@ -2,6 +2,9 @@
 # coding=utf-8
 import nfqueue, socket
 from scapy.all import *
+import subprocess
+
+liste_addr=[]
 
 def traite_paquet(payload):
     # le paquet est fourni sous forme d'une séquence d'octet, il faut l'importer
@@ -12,15 +15,60 @@ def traite_paquet(payload):
         # paquet IPv4
         pkt = IP(data)
     else:
-        # paquet IPv6
+        # paquet IPv6 to ipv4
         pkt = IPv6(data)
-    pkt.show()
+        cmd = subprocess.Popen("sudo ip netns exec hote1 ip a show dev hote1-eth0 | grep 'ether' | cut -b 16-33", shell=True,stdout=subprocess.PIPE)
+        (addr_ether_src, ignorer) = cmd.communicate()
+        cmd = subprocess.Popen("ip a show dev switchipv6 | grep 'ether' | cut -b 16-33", shell=True,stdout=subprocess.PIPE)
+        (addr_ether_dst, ignorer) = cmd.communicate()
+        ether=Ether()
+        ether.dst= str(addr_ether_dst.decode()).strip()
+        ether.src=str(addr_ether_src.decode()).strip()
+        ether.type=0x800
+        addr_ipv4_dst =traducteur(pkt.dst,6)
+        if addr_ipv4_dst:
+            # sauvegarder la correspondance ipv6 to ipv4
+            liste_addr.append((pkt.dst,addr_ipv4_dst))
+            cmd = subprocess.Popen("ip a show dev switchipv6 | grep 'inet ' | cut -b 10-24", shell=True,stdout=subprocess.PIPE)
+            (addr_ipv4_src, ignorer) = cmd.communicate()
+
+            ip4 = IP()
+            ip4.dst=addr_ipv4_dst
+            ip4.src=str(addr_ipv4_src.decode()).strip()
+            # faire le paquet
+            pkt4=ether/ip4/pkt[TCP]
+
+            pkt4.show2()
+            # si modifie : le paquet est remis MODIFIE dans la pile TCP/IP et poursuit sa    route
+            payload.set_verdict_modified(nfqueue.NF_ACCEPT, bytes(pkt4), len(pkt4))
+            ## pour envoyer un datagramme IP sur l'interface eth0
+            send(pkt4,iface="wlp0s20f3")
+        else:
+            # si rejete : le paquet est rejeté
+            payload.set_verdict(nfqueue.NF_DROP)
+
+    #pkt.show()
     # accepte le paquet : le paquet est remis dans la pile TCP/IP et poursuit sa route
     #payload.set_verdict(nfqueue.NF_ACCEPT)
-    # si modifie : le paquet est remis MODIFIE dans la pile TCP/IP et poursuit sa    route
-    #payload.set_verdict_modified(nfqueue.NF_ACCEPT, bytes(pkt), len(pkt))
-    # si rejete : le paquet est rejeté
-    #payload.set_verdict(nfqueue.NF_DROP)
+
+
+
+def traducteur(ipx,type):
+    if type==4:
+        cmd = subprocess.Popen("dig -x %s +short"%ipx, shell=True,stdout=subprocess.PIPE)
+        (dns, ignorer) = cmd.communicate()
+        cmd = subprocess.Popen("dig -t aaaa %s +short"%str(dns.decode()).strip(), shell=True,stdout=subprocess.PIPE)
+        (addr_ipv6, ignorer) = cmd.communicate()
+        return addr_ipv6.decode().split("\n")[0]
+    if type==6:
+        cmd = subprocess.Popen("dig -x %s +short"%ipx, shell=True,stdout=subprocess.PIPE)
+        (dns, ignorer) = cmd.communicate()
+        cmd = subprocess.Popen("dig -t a %s +short"%str(dns.decode()).strip(), shell=True,stdout=subprocess.PIPE)
+        (addr_ipv4, ignorer) = cmd.communicate()
+        #print(repr(addr_ipv4.decode()))
+        return addr_ipv4.decode().split("\n")[0]
+
+
 
 
 
@@ -34,8 +82,8 @@ q.set_callback(traite_paquet)
 q.create_queue(0)
 try:
     q.try_run()
-except KeyboardInterrupt, e:
-    print "interruption"
+except KeyboardInterrupt(e):
+    print("interruption")
 q.unbind(socket.AF_INET)
 q.unbind(socket.AF_INET6)
 q.close()
---
+++ source/audit_test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+
+import subprocess
+from scapy.all import *
+
+
+def traiter_packet(p):
+    if IPv6 in p:
+        if TCP in p:
+            p.show2()
+
+
+    ## dig ipv4 to ipv6
+    # dig -x 74.125.236.167 +short
+    # dig -t aaaa dns.google +short
+
+    ## dig ipv6 to ipv4
+    #dig -x 2001:4860:4860::8844 +short
+    #dig -t a dns.google +short
+
+    #Netfilter
+    #sudo iptables -t mangle -A PREROUTING -p tcp --sport 7890 -j NFQUEUE --queue-num 0
+    #sudo ip6tables -t mangle -A PREROUTING -i switchipv6 -p tcp --dport 7890 -j NFQUEUE --queue-num 0
+
+    # socat - tcp6-listen:7890
+    # socat - tcp6:[2001:41d0:2:eb43::b7f9:7a68]:7890
+    # socat - tcp6:[fe80::e02b:17ff:fe97:db47%eth0]:7890
+
+
+    # pour envoyer une trame ethernet sur l'interface bridge_ipv6
+    #sendp(pkt,iface="bridge_ipv6")
+
+    # type ether
+    #  ipv4 0x800
+    # IPv6  0x86DD
+
+    # recuperer les ethernet @
+    # sudo ip netns exec hote1 ip a show dev <interface> | grep "ether" | cut -b 16-33
+sniff(count=0,prn=traiter_packet,iface=["switchipv6"])
